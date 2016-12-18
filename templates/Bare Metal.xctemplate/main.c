f933834248c356b47f251b23319bc51a2563e898
Heiko Abraham <abrahamh@web.de>
2016-12-18 15:41:42 +0100

    add initial templates

---
+++ templates/Bare Metal.xctemplate/main.c
@@ -0,0 +1,28 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#include <stdio.h>
+
+void Reset_Handler(void);
+int main(int argc, const char * argv[]);
+
+void Reset_Handler(void)
+{
+   /* todo : setup ISR */
+   /* todo : setup clock */
+   /* todo : setup bss / data / RAM */
+   
+   main(0, NULL);
+}
+
+
+
+int main(int argc, const char * argv[])
+{
+	return 0;
+}
